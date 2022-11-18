e5a600f30d210d6c3869ae63a86813239e1d3921
obaid147 <obaidfayazwani@gmail.com>
2022-11-18 17:25:05 +0530

    A custom FLOW - a simple filter flow as exercise, Materialized values in graph stages using GraphStageWithMaterializedValue[] and Flow that counts number of elements that go through it where we use map for upstream and not for downstream as it cancels/terminates stream once it hits a exception

--- src/main/scala/part5_advanced/CustomOperators.scala
+++ src/main/scala/part5_advanced/CustomOperators.scala
@@ -1,12 +1,13 @@
 package part5_advanced
 
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.{Sink, Source}
-import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
+import akka.stream.stage.{GraphStage, GraphStageLogic, GraphStageWithMaterializedValue, InHandler, OutHandler}
 import akka.stream._
 
 import scala.collection.mutable
-import scala.util.Random
+import scala.concurrent.{Future, Promise}
+import scala.util.{Failure, Random, Success}
 
 object CustomOperators extends App {
 
@@ -38,7 +39,7 @@ object CustomOperators extends App {
     }
   }
 
-  val randomGeneratorSource = Source.fromGraph(new RandomNumberGenerator(10))
+  val randomGeneratorSource = Source.fromGraph(new RandomNumberGenerator(100))
 
 //  val materializingRandomGeneratorSource =
 //    randomGeneratorSource.runWith(Sink.foreach[Int](println))
@@ -94,7 +95,7 @@ object CustomOperators extends App {
 
   val batcherSink = Sink.fromGraph(new Batcher(10))
 
-  randomGeneratorSource.to(batcherSink).run()
+  //randomGeneratorSource.to(batcherSink).run()
   /** Noting happens when we do this because:-
    * We are connecting batcherSink to a randomGeneratorSource where
    * randomGeneratorSource is waiting for upstream to send an element, (chicken and egg problem)
@@ -143,4 +144,105 @@ object CustomOperators extends App {
     isAvailable:- Checks if an element has already been pushed to the port.
     isClosed:-  checks if closed or not
    */
+
+
+  /** #3 A custom FLOW - a simple filter flow*/
+  class FilterFlow[T](predicate: T => Boolean) extends GraphStage[FlowShape[T, T]] {
+    val inPort: Inlet[T] = Inlet[T]("filterIN")
+    val outPort: Outlet[T] = Outlet[T]("filterOut")
+
+    override def shape: FlowShape[T, T] = FlowShape(inPort, outPort)
+
+    override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogic(shape) {
+      setHandler(outPort, new OutHandler {
+        override def onPull(): Unit = pull(inPort)
+      })
+      setHandler(inPort, new InHandler {
+        override def onPush(): Unit = {
+          try {
+            val nxtEle = grab(inPort)
+            if (predicate(nxtEle)) push(outPort, nxtEle) // if predicate is met, pass ele  on
+            else pull(inPort) // otherwise ask for another element.
+
+          } catch {
+            case e: Throwable => failStage(e)
+          }
+        }
+      })
+    }
+  }
+
+  val myFilter = Flow.fromGraph(new FilterFlow[Int](_ > 50))
+  randomGeneratorSource.via(myFilter).to(batcherSink)//.run()
+  // backpressure also works out of the box.... emits 10 elements per second
+
+
+  /** Materialized values in graph stages*/
+  /** #4 Flow that counts number of elements that go through it*/
+  class CounterFlow[T] extends GraphStageWithMaterializedValue[FlowShape[T, T], Future[Int]] {
+
+    val inPort: Inlet[T] = Inlet[T]("counterIN")
+    val outPort: Outlet[T] = Outlet[T]("counterOut")
+
+    override val shape: FlowShape[T, T] = FlowShape(inPort, outPort)
+    // val because we don't want shape to be evaluated everytime...
+
+    override def createLogicAndMaterializedValue(inheritedAttributes: Attributes): (GraphStageLogic, Future[Int]) = {
+      val promise: Promise[Int] = Promise[Int]
+      val logic: GraphStageLogic = new GraphStageLogic(shape){
+        // setting mutable state
+        var counter = 0
+        setHandler(outPort, new OutHandler {
+          override def onPull(): Unit = pull(inPort)
+
+          override def onDownstreamFinish(): Unit = {
+            promise.success(counter)
+            super.onDownstreamFinish()
+          }
+        })
+        setHandler(inPort, new InHandler {
+          override def onPush(): Unit = {
+            // extract the element
+            val nxtEle = grab(inPort)
+            counter += 1
+            push(outPort, nxtEle)
+          }
+
+          override def onUpstreamFinish(): Unit = {
+            promise.success(counter)
+            super.onUpstreamFinish()
+          }
+
+          override def onUpstreamFailure(ex: Throwable): Unit = {
+            promise.failure(ex)
+            super.onUpstreamFailure(ex)
+          }
+        })
+      }
+      (logic, promise.future)
+    }
+  }
+
+  val counterFlow: Flow[Int, Int, Future[Int]] = Flow.fromGraph(new CounterFlow[Int])
+  val countFuture: Future[Int] = Source(1 to 10)
+    //.map(x => if(x == 8) throw new RuntimeException("Reached 8") else x)// upStream
+    .viaMat(counterFlow)(Keep.right)
+    //.to(Sink.foreach[Int](println))// if upstream fails, onUpStreamFailure
+    .to(Sink.foreach[Int]
+      //if downstream fails onDownstreamFinish is called, simply terminates stream as no onDownStreamFailure method.
+      (x => if(x == 8) throw new RuntimeException("Sink at 8") else println(x)))
+    .run()
+
+  import system.dispatcher
+  countFuture.onComplete{
+    case Success(count) => println(s"Total Number of elements passed = $count")
+    case Failure(ex) => println(s"Counting failed coz:- $ex")
+  }
+
+  /** Handler Callbacks onPush onPull etc... are never call concurrently.
+   *  so, we can safely access mutable states inside these handlers......
+   *
+   *  Drawback is that We should never ever expose mutable states outside these Handlers,
+   *  eg: Future onComplete callbacks, this can break component encapsulation
+   *      much the same way as we can break Actor encapsulation*/
 }
